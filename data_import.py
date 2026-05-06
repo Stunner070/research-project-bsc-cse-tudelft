733a98b639ebe9b7cb1dccab540d082857ae765b
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-06 20:05:03 +0200

    added dataimport code from scratch and added yaml to requirements.txt

---
+++ data_import.py
@@ -0,0 +1,7 @@
+import yaml
+
+with open("configs/default.yaml", "r") as f:
+    cfg = yaml.safe_load(f)
+
+DATA_ROOT = cfg["data_root"]
+print("Using data from:", DATA_ROOT)
--- init.py
+++ init.py
@@ -1,7 +1,3 @@
-#create a demo file outputting the current date and time
-import datetime
-def main():
-    now = datetime.datetime.now()
-    with open("demo.txt", "w") as f:
-        f.write("Current date and time: " + str(now))
-if __name__ == "__main__":    main()
+import data_import as di
+
+print("Data root:", di.DATA_ROOT)
