48515bc0e593d0d951f11362a0a14bad5976ab40
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-07 15:13:50 +0200

    add videos

--- data_import.py
+++ data_import.py
@@ -1,12 +1,3 @@
-# import yaml
-#
-# with open("configs/default.yaml", "r") as f:
-#     cfg = yaml.safe_load(f)
-#
-# DATA_ROOT = cfg["data_root"]
-# print("Using data from:", DATA_ROOT)
-
-import os
 from pathlib import Path
 
 DATA_ROOT = Path("/scratch/sofyanali/celebvhq/videos")
--- init.py
+++ init.py
@@ -1,10 +1,3 @@
-from pathlib import Path
+from data_import import DATA_ROOT
 
-root = Path("/scratch/sofyanali/celebvhq/videos")
-print("Exists:", root.exists())
-print("MP4 count:", len(list(root.rglob("*.mp4"))))
-
-#output to txt file
-with open("data_paths.txt", "w") as f:
-    f.write(f"Exists: {root.exists()}\n")
-    f.write(f"MP4 count: {len(list(root.rglob('*.mp4')))}\n")
+print(f"Data root is set to: {DATA_ROOT}")
