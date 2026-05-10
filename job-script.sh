c4ffb932ad9cfd67b263b7161d102a616319407c
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-10 11:56:01 +0200

    change job script

--- job-script.sh
+++ job-script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=testrun
-#SBATCH --partition=compute
+#SBATCH --partition=gpu
 #SBATCH --account=education-eemcs-courses-cse3000
 #SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=2GB
+#SBATCH --mem-per-cpu=1GB
 #SBATCH --time=00:10:00
 
 srun source ~/projects/.venv/bin/activate
