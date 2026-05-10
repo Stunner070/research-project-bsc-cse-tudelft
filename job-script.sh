8abebd30b11eac668612752b84a8a7a00a994b7f
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-10 11:46:21 +0200

    change job script

--- job-script.sh
+++ job-script.sh
@@ -4,10 +4,9 @@
 #SBATCH --account=education-eemcs-courses-cse3000
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=1GB
+#SBATCH --mem-per-cpu=2GB
 #SBATCH --time=00:10:00
 
 srun source ~/projects/.venv/bin/activate
 
-srun cd ~/projects/rpbsc/src/scripts
-srun python run_full_pipeline.py --mode event_frames_only  --device cuda --max_samples 1
+srun python ~/projects/rpbsc/src/scripts/run_full_pipeline.py --mode event_frames_only  --device cuda --max_samples 1
