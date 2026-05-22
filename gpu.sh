fd4cc5ba22a13ad6730957e2833c9889fa8c2d1a
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-22 21:32:13 +0200

    change confiig

--- gpu.sh
+++ gpu.sh
@@ -10,4 +10,4 @@
 
 . /scratch/sofyanali/.venv/bin/activate
 
-srun python /scratch/sofyanali/rpbsc/src/scripts/run_full_pipeline.py --mode all  --device cuda --backbone facenet --privacy_eval
+srun python /scratch/sofyanali/rpbsc/src/scripts/run_full_pipeline.py --mode event_frames_only  --device cuda --backbone facenet --privacy_eval
