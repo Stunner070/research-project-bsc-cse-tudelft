2903cefbaa8e6f743883703aa7bb48aaa856a659
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-23 16:45:24 +0200

    chagne bash

--- gpu.sh
+++ gpu.sh
@@ -13,7 +13,5 @@ module load python/3.11.9
 
 . /scratch/sofyanali/.venv/bin/activate
 
-srun python -m pip install insightface onnxruntime-gpu
-
 #srun python /scratch/sofyanali/rpbsc/src/scripts/run_full_pipeline.py --mode event_frames_only  --device cuda --backbone facenet --privacy_eval
 srun python /scratch/sofyanali/rpbsc/run_retrieval_pipeline.py
