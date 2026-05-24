fb909d8dafecdb777be062729fdf9678c7b247c2
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-24 22:22:38 +0200

    change config cpu

--- cpu.sh
+++ cpu.sh
@@ -7,7 +7,11 @@
 #SBATCH --mem-per-cpu=3GB
 #SBATCH --time=03:50:00
 
+module load 2025
+module load python/3.11.9
+module load py-scikit-learn
+
 . /scratch/sofyanali/.venv/bin/activate
 
 #srun python /scratch/sofyanali/rpbsc/src/scripts/run_full_pipeline.py --mode event_frames_only --backbone facenet --privacy_eval
-srun python /scratch/sofyanali/rpbsc/run_retrieval_pipeline.py
+srun /scratch/sofyanali/.venv/bin/python /scratch/sofyanali/rpbsc/run_retrieval_pipeline.py
