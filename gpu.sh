7fb1a80c2c5362a48f5432386ff25028d545c48b
Sofyan Ali Ali <s.a.ali-2@student.tudelft.nl>
2026-05-26 20:48:58 +0200

    change batch

--- gpu.sh
+++ gpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=cutoff_100_300clips
-#SBATCH --partition=gpu
+#SBATCH --partition=gpu-a100-small
 #SBATCH --account=education-eemcs-courses-cse3000
 #SBATCH --cpus-per-task=2
 #SBATCH --gpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=6GB
-#SBATCH --time=05:00:00
+#SBATCH --time=04:00:00
 
 module load 2025
 module load python/3.11.9
